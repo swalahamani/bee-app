d8589ea36eee5a03cc2d73f5cb58f243f5435fcd
Muhammad Swalah A A <mail@swalahamani.com>
2021-07-13 22:49:53 +0530

    update NetworkTypes - add APIResponse & httpStatusCodes

--- src/config/NetworkTypes.ts
+++ src/config/NetworkTypes.ts
@@ -7,6 +7,10 @@
 import {NullableString} from "@config/CommonTypes";
 
 type APIResponseMessageType = NullableString;
+type APIResponse = {
+	error: any;
+	httpStatuCode: httpStatusCodes | null;
+};
 
 /**
  * The standard api response which will be used throuout the application
@@ -32,6 +36,29 @@ enum apiResponseStatuses {
 	ERROR = "ERROR",
 }
 
-export type {APIResponseMessageType};
+/**
+ * All the http status codes returned by the api server needs to be defiend here.
+ * This is used for determining the type/status of the api response after the call.
+ *
+ * Refer: https://www.restapitutorial.com/httpstatuscodes.html
+ */
+enum httpStatusCodes {
+	// 2xx SUCCESS
+	SUCCESS_OK = 200,
+	SUCCESS_CREATED = 201,
+	SUCCESS_NO_CONTENT = 204,
 
-export {apiResponseStatuses};
+	// 4xx CLIENT_ERROR
+	CLIENT_ERROR_BAD_REQUEST = 400,
+	CLIENT_ERROR_UNAUTHORIZED = 401,
+	CLIENT_ERROR_FORBIDDEN = 403,
+	CLIENT_ERROR_NOT_FOUND = 404,
+
+	// 5xx SERVER_ERROR
+	SERVER_ERROR_INTERNAL_SERVER_ERROR = 500,
+	SERVER_ERROR_SERVICE_UNAVAILABLE = 503,
+}
+
+export type {APIResponseMessageType, APIResponse};
+
+export {apiResponseStatuses, httpStatusCodes};
