10e18384df936d27e2968f0abcab5f7974da1ff5
Muhammad Swalah A A <mail@swalahamani.com>
2021-07-13 19:37:50 +0530

    update HomeScreen: 	- convert to Class Component 	- add getStyles for handling dynamic style sheets depending on the device-orientation 	- define and implement navigation prop type

--- src/screens/homescreen/HomeScreen.tsx
+++ src/screens/homescreen/HomeScreen.tsx
@@ -3,28 +3,60 @@
  *  Created On : Sat Jul 10 2021
  *  File : HomeScreen.tsx
  *******************************************/
-import React, {FC} from "react";
+import React, {Component} from "react";
 import {SafeAreaView, Text} from "react-native";
 
-type Props = {};
+import {StackNavigationProp} from "@react-navigation/stack";
 
-const defaultProps: Partial<Props> = {};
+import {AppRouteBottomTabParamList} from "@config/constants/NavConstants";
 
-const HomeScreen: FC<Props> = () => {
-	return (
-		<SafeAreaView
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Home Screen</Text>
-		</SafeAreaView>
-	);
+import PortStyles from "./PortStyles";
+
+type HomeScreenNavigationProp = StackNavigationProp<
+	AppRouteBottomTabParamList,
+	"HomeScreen"
+>;
+
+type Props = {
+	navigation: HomeScreenNavigationProp;
 };
 
-HomeScreen.displayName = "HomeScreen";
-HomeScreen.defaultProps = defaultProps;
+type State = {};
+
+class HomeScreen extends Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	/**
+	 * Function which returns the appropriate style file depending on different
+	 * screen orientation.
+	 *
+	 * NOTE: Currently it is directly returning the Styles.s as the application only
+	 * supports portrait orientation. In future, while adding support for landscape orientation,
+	 * will change the implementation for returning the landscape style-sheet file
+	 * by listening to the orientation change.
+	 * @returns
+	 */
+	getStyles = () => {
+		return PortStyles;
+	};
+
+	render() {
+		return (
+			<SafeAreaView
+				style={{
+					flex: 1,
+					justifyContent: "flex-start",
+					alignItems: "flex-start",
+				}}
+			>
+				<Text>Home Screen Label</Text>
+			</SafeAreaView>
+		);
+	}
+}
 
 export default HomeScreen;
---
+++ src/screens/homescreen/PortStyles.ts
@@ -0,0 +1,12 @@
+/******************************************
+ *  Author : Muhammad Swalah A A
+ *  Created On : Tue Jul 13 2021
+ *  File : PortStyles.ts
+ *******************************************/
+import {StyleSheet} from "react-native";
+
+import {} from "react-native-responsive-screen";
+
+export default StyleSheet.create({
+	container: {},
+});
