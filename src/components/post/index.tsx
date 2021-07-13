0fe730e8d179924bc4317359e67baa83f9249b60
Muhammad Swalah A A <mail@swalahamani.com>
2021-07-13 20:32:47 +0530

    add Post component

---
+++ src/components/post/index.tsx
@@ -0,0 +1,8 @@
+/******************************************
+ *  Author : Muhammad Swalah A A
+ *  Created On : Tue Jul 13 2021
+ *  File : index.tsx
+ *******************************************/
+import Post from "./Post";
+
+export default Post;
